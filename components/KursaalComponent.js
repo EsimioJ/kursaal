068072674d0095a3bde2543e3727e1d8fae3c682
Jacopo Riccioni <EsimioJ@users.noreply.github.com>
2022-12-10 12:14:52 +0100

    quasi ok

--- components/KursaalComponent.js
+++ components/KursaalComponent.js
@@ -7,7 +7,7 @@ import data from "./data";
 
 const KursaalComponent = () => {
   const filmArray = data.films;
-  //console.log(filmArray);
+  console.log(filmArray);
   filmArray.sort((a, b) => {
     a = new Date(a.film_occupations[0].start);
     a = a.getTime();
@@ -17,32 +17,9 @@ const KursaalComponent = () => {
     return a - b;
   });
 
-  // const sortedFilms = data.films.sort(
-  //   (a, b) =>
-  //     // new Date(b.film_occupations[0].start) - Date(a.film_occupations[0].start)
-  //     a.getTime(film_occupations[0].start) -
-  //     b.getTime(film_occupations[0].start)
-  // );
-  // console.log(sortedFilms);
-  // for (let i=0; i<data.films.length; i++){
-  //   let a = new Date(data.films[i].film_occupations[0].start);
-  //   let b = new Date(data.films[i+1].film_occupations[0].start);
-  //   console.log(a.getTime());
-  //   console.log(b.getTime());
-  // }
-
   const [films, setFilms] = useState(data.films);
   const [active, setActive] = useState(0);
 
-  //console.log(films);
-  // const getData = async () => {
-  //   const response = await axios.get(url);
-  //   //console.log(response);
-  //   setFilms(response.data);
-  // };
-  // useEffect(() => {
-  //   getData();
-  // }, []);
   return (
     <>
       <ul className="films w-full flex flex-row mx-4 overflow-x-scroll">
@@ -75,11 +52,11 @@ const KursaalComponent = () => {
           );
         })}
       </ul>
-      <div class="w-full flex justify-center items-center mt-8">
-        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-0 px-2 rounded-l">
+      <div className="w-full flex justify-center items-center mt-8">
+        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-0 px-2 rounded-l">
           Prev
         </button>
-        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-0 px-2 rounded-r">
+        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-0 px-2 rounded-r">
           Next
         </button>
       </div>
@@ -87,10 +64,7 @@ const KursaalComponent = () => {
   );
 };
 
-
-
-
- function Spettacolo({
+function Spettacolo({
   title,
   id,
   length,
@@ -103,66 +77,60 @@ const KursaalComponent = () => {
   film_occupations,
 }) {
   return (
-    <div>
+    <div className="flex">
       <div className=" basis-1/3 p-4">
-                <img src={img} alt={title} />
-              </div>
-              <div className=" basis-2/3 p-4 mt-2">
-                <h2 className=" text-3xl">{title}</h2>
+        <img src={img} alt={title} />
+      </div>
+      <div className=" basis-2/3 p-4 mt-2">
+        <h2 className=" text-3xl">{title}</h2>
 
-                <h4 className="text-lg mt-2">
-                  <b>Regia:</b> {director}
-                </h4>
-                <h6>
-                  <b>Cast:</b> {cast}
-                </h6>
-                <p>
-                  <b>Durata:</b>
-                  {length} minuti
-                </p>
-                <h2 className=" text-lg mt-2">
-                  <b>Spettacoli:</b>
-                </h2>
-                <ul>
-                  {film_occupations.map((spettacolo) => {
-                    let spettacoloD = new Date(spettacolo.start);
-                    spettacoloD = spettacoloD.toLocaleDateString();
-                    let spettacoloO = new Date(spettacolo.start);
-                    spettacoloO = spettacoloO.toLocaleTimeString();
-                    //console.log(spettacoloD, spettacoloO);
-                    return (
-                      <li
-                        className="mt-2"
-                        key={spettacolo.projection_public_id}
-                      >
-                        {spettacoloD} - {spettacoloO}{" "}
-                        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
-                          Acquista
-                        </button>
-                        <TicketIcon className="h-6 w-6 text-red-500 hover:text-red-700" />
-                      </li>
-                    );
-                  })}
-                </ul>
-                <p className="mt-2">
-                  <b>Trama:</b>
-                  <br />
-                  {plot}
-                </p>
-
-                <button
-                  className="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                  href={`https://www.youtube.com/watch?v=` + url_trailer}
-                  target="_blank"
-                >
-                  Trailer
+        <h4 className="text-lg mt-2">
+          <b>Regia:</b> {director}
+        </h4>
+        <h6>
+          <b>Cast:</b> {cast}
+        </h6>
+        <p>
+          <b>Durata:</b>
+          {length} minuti
+        </p>
+        <h2 className=" text-lg mt-2">
+          <b>Spettacoli:</b>
+        </h2>
+        <ul>
+          {film_occupations.map((spettacolo) => {
+            let spettacoloD = new Date(spettacolo.start);
+            spettacoloD = spettacoloD.toLocaleDateString();
+            let spettacoloO = new Date(spettacolo.start);
+            spettacoloO = spettacoloO.toLocaleTimeString();
+            //console.log(spettacoloD, spettacoloO);
+            return (
+              <li className="mt-2" key={spettacolo.projection_public_id}>
+                {spettacoloD} - {spettacoloO}{" "}
+                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+                  Acquista
                 </button>
-              </div>
+                <TicketIcon className="h-6 w-6 text-red-500 hover:text-red-700" />
+              </li>
+            );
+          })}
+        </ul>
+        <p className="mt-2">
+          <b>Trama:</b>
+          <br />
+          {plot}
+        </p>
+
+        <button
+          className="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          href={`https://www.youtube.com/watch?v=` + url_trailer}
+          target="_blank"
+        >
+          Trailer
+        </button>
+      </div>
     </div>
-  )
+  );
 }
-
-
-
 
 export default KursaalComponent;
--- components/avatar.js
+++ components/avatar.js
@@ -1,23 +1,25 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Avatar({ author }) {
   const name = author
     ? author.node.firstName && author.node.lastName
       ? `${author.node.firstName} ${author.node.lastName}`
       : author.node.name
-    : null
+    : null;
 
   return (
     <div className="flex items-center">
       <div className="w-12 h-12 relative mr-4">
         <Image
           src={author.node.avatar.url}
-          layout="fill"
+          style="fill"
           className="rounded-full"
           alt={name}
+          width="80"
+          height="80"
         />
       </div>
       <div className="text-xl font-bold">{name}</div>
     </div>
-  )
+  );
 }
